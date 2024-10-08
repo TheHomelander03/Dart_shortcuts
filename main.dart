94070de2de22baf0abb91e30550c39be183e6519
TheHomelander03 <reyjeffgc0785@gmail.com>
2024-10-08 12:10:35 -0400

    Interpolacion de variables

---
+++ main.dart
@@ -0,0 +1,14 @@
+void main(List<String> args) {
+  String name = "Miguel";
+  int age = 21;
+
+  //Basic
+  print("Hello world my name is $name and i have $age years old");
+
+  //mid
+
+  print("hello world my name is $name and i have ${11 + 10} years old");
+
+  //Tupper
+  print("Hello world my names is ${name.toUpperCase()}");
+}
