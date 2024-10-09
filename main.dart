79436433fafe6a1bf720b303045f19cada7ded4f
TheHomelander03 <reyjeffgc0785@gmail.com>
2024-10-09 12:05:12 -0400

    Tipos de colecciones

--- main.dart
+++ main.dart
@@ -13,11 +13,31 @@ void main(List<String> args) {
   print("Hello world my names is ${name.toUpperCase()}");
 
   //Variables con posibilad nula
-  String? name1 = null;
+  String? name1;
 
   print("El nombre es ${name1 ?? 'Invitado'}");
 
   int? age1;
 
   print("Mi edad es de ${age1 ?? 21}");
+
+  //Tipos de colecciones
+
+  List<int?> numbers = [12, 03, null, 21];
+  print("Number list: ");
+  numbers.forEach((num) => print(num ?? 'null value'));
+
+  Set<String?> names = {"Miguel", "Anibal", null, "Homelander"};
+  print("The name set: ");
+  names.forEach((names) => print(names ?? 'null value'));
+
+  Map<String, int?> info = {
+    'Miguel': 21,
+    'Homelander': 41,
+    'Julius': null,
+    'Jose': 50
+  };
+  print("Map list:");
+  info.forEach((Names2, Age2) =>
+  print("Name: $Names2, Age: ${Age2 ?? "No avaidable"}" ));
 }
